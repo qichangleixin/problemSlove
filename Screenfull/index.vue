2de3492785d31b912f98f2081f0b9216c9a1a5b0
qichangleixin <737546268@qq.com>
2021-06-25 16:50:29 +0800

    Create index.vue
    vue 全屏组件封装
    "screenfull": "4.2.0"

---
+++ Screenfull/index.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <svg-icon :icon-class="isFullscreen?'exit-fullscreen':'fullscreen'" @click="click" />
+  </div>
+</template>
+
+<script>
+import screenfull from 'screenfull'
+
+export default {
+  name: 'Screenfull',
+  data() {
+    return {
+      isFullscreen: false
+    }
+  },
+  props: {
+    targetId: {
+      type: String,
+      required: false
+    }
+  },
+  mounted() {
+    this.init()
+  },
+  beforeDestroy() {
+    this.destroy()
+  },
+  methods: {
+    click() {
+      if (!screenfull.enabled) {
+        this.$message({
+          message: 'you browser can not work',
+          type: 'warning'
+        })
+        return false
+      }
+      if (this.targetId) {
+        const bodyEle = document.getElementById(this.targetId)
+        screenfull.toggle(bodyEle)
+      } else {
+        screenfull.toggle()
+      }
+    },
+    change() {
+      this.isFullscreen = screenfull.isFullscreen
+      this.$emit('fullScreenChange', this.isFullscreen)
+    },
+    init() {
+      if (screenfull.enabled) {
+        screenfull.on('change', this.change)
+      }
+    },
+    destroy() {
+      if (screenfull.enabled) {
+        screenfull.off('change', this.change)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.screenfull-svg {
+  display: inline-block;
+  cursor: pointer;
+  fill: #5a5e66;;
+  width: 20px;
+  height: 20px;
+  vertical-align: 10px;
+}
+</style>
